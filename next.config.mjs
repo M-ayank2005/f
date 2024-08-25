9ef80276c1f9a5f3d5228f8b3116d60f91370467
Mayank Mishra <mrmishramayank2005@gmail.com>
2024-08-25 15:40:15 +0530

    Initial commit

---
+++ next.config.mjs
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+
+    // next.config.js
+    images: {
+      domains: ['images.unsplash.com','ih0.redbubble.net'],
+      
+    },
+    trailingSlash: true,
+   
+  
+};
+
+export default nextConfig;
